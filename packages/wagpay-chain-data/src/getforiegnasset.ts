c83b4bd930ec3daf60c96ad30f4cd0958339be2c
vikash <44chansw@gmail.com>
2022-09-13 01:06:50 +0530

    get forign assets

---
+++ packages/wagpay-chain-data/src/getforiegnasset.ts
@@ -0,0 +1,30 @@
+import { getToken, getTokenByName } from ".";
+
+const chain_id: any = {
+  "1": "1",
+  "2": "137",
+  "3": "43114",
+  "4": "56",
+};
+
+export const find_foreign_token = (
+  token_address: string,
+  from_chain: string,
+  to_chain: string
+) => {
+  const token = getToken(token_address, from_chain);
+
+  if (token.extensions) {
+    if (token.extensions.bridgeInfo) {
+      const t = token.extensions.bridgeInfo[chain_id[to_chain]];
+
+      if (t) {
+        return t.tokenAddress;
+      }
+    }
+  }
+
+  return to_chain === "3"
+    ? getTokenByName("BUSD", to_chain).address
+    : getTokenByName("USDC", to_chain).address;
+};
--- packages/wagpay-chain-data/src/index.ts
+++ packages/wagpay-chain-data/src/index.ts
@@ -1,64 +1,66 @@
-import chains from "../chain.json"
-import {tokens} from "../tokens"
+import chains from "../chain.json";
+import { tokens } from "../tokens";
 
 export interface Token {
-    name: string
-    symbol: string
-    decimals: number
+  name: string;
+  symbol: string;
+  decimals: number;
 }
 
 export interface Explorer {
-    name: string
-    url: string
-    standard: string
+  name: string;
+  url: string;
+  standard: string;
 }
 
 export interface Chain {
-    internalId: number
-    chainId: number
-    name: string
-    icon: string
-    explorers: Explorer[]
-    chainType: string
-    rpcUrls: string[]
-    tokens: { [key: string]: Token }
-    nativeCurrency: Token
-    faucet: string
-    shortName: string
+  internalId: number;
+  chainId: number;
+  name: string;
+  icon: string;
+  explorers: Explorer[];
+  chainType: string;
+  rpcUrls: string[];
+  tokens: { [key: string]: Token };
+  nativeCurrency: Token;
+  faucet: string;
+  shortName: string;
 }
 
-export const chainData: Chain[] = (chains as unknown) as Chain[]
+export const chainData: Chain[] = chains as unknown as Chain[];
 
+export const getChain = (params: { internalId?: number; chainId?: number }) => {
+  if (!params.internalId && !params.chainId) {
+    throw "either send internalId or chainId";
+  }
 
-export const getChain = (params: { internalId?: number, chainId?: number }) => {
-    if(!params.internalId && !params.chainId) {
-        throw "either send internalId or chainId"
-    }
-
-    if(params.internalId) {
-        return chainData.find(chain => chain.internalId === params.internalId)
-    } else {
-        return chainData.find(chain => chain.chainId === params.chainId)
-    }
-}
+  if (params.internalId) {
+    return chainData.find((chain) => chain.internalId === params.internalId);
+  } else {
+    return chainData.find((chain) => chain.chainId === params.chainId);
+  }
+};
 
 export const getChains = () => {
-    return chainData
-}
+  return chainData;
+};
 
 export const getTokens = (internalId?: number) => {
-    if(!internalId) {
-        return tokens
-    }
+  if (!internalId) {
+    return tokens;
+  }
 
-    return tokens[internalId]
-}
-
+  return tokens[internalId];
+};
 
 export const getToken = (address: string, chain: string) => {
-    const allTokens = tokens[chain]
-    
-    return allTokens.find((token: any) => token.address.toLowerCase() === address.toLowerCase())
-}
+  const allTokens = tokens[chain];
 
-export * from "./getInternalChainId"
+  return allTokens.find(
+    (token: any) => token.address.toLowerCase() === address.toLowerCase()
+  );
+};
+
+export declare const getTokenByName: (name: string, chain: string) => any;
+
+export * from "./getInternalChainId";
